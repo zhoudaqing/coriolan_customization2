6848da9e19ef3efda7d29ca65ee90f3d6a6ea2eb
OneataBogdan <bogdantm1@gmail.com>
2014-12-26 09:48:25 +0200

    improved-js-init

--- js/carousel/improved_versions_not_working/carousel_13.js
+++ js/carousel/improved_versions_not_working/carousel_13.js
@@ -1,23 +1,27 @@
  //dev version
  //change the hardcoding
  $(document).ready(function(){
-     console.log('carousel_13 1 cache');
+     console.log('carousel_13 11 cache');
      var id_recently_click='#sw_dropdown_269';
      var id_recently_block='#dropdown_269';
      var id_favorite_click='#sw_dropdown_279';
      var id_favorite_block='#dropdown_279';
      function carousel_footer(id_click, id_block,bigDiv,smallDiv) {
-          $(id_click).on('click',function(){ //click to show dropup
+      //    $(id_click).on('click',function(){ //click to show dropup
             setTimeout(function(){
                   sliderUl=$(id_block).find('div.lsc_slider').css('overflow','hidden').children('ul'),
-                  imgs=$(id_block).find('.lsc_li_container'),
+                  imgs=sliderUl.find('.lsc_li_container').filter(":visible"),
                   imgWidth=$('ul.recent_carousel_ul.lcs_fix li.clearfix.lsc_li_container').filter(":visible").first().outerWidth(true), //hardcoded width because of firefox bug(initial width 190)
                   imgsLen=imgs.length,
                   current=1, //remove
                   totalImgsWidth=imgsLen * imgWidth,
                   sliderLoc=1;
                   bigSlide=  adjustCarousel(imgWidth,bigDiv,smallDiv)/*11*/;
-                 console.log('bigSlide='+bigSlide+';imgWidth='+imgWidth/*+'; imgsLen='+imgsLen+'; sliderLoc='+sliderLoc+'; totalImgsWidth='+totalImgsWidth*/);
+                  sliderUl.animate({
+                  'margin-left': 0
+                    })
+                  console.log('BIGdIV, SMALLdIV: ',bigDiv,smallDiv);
+                 console.log('bigSlide='+bigSlide+';imgsLen='+imgsLen/*+'; imgsLen='+imgsLen+'; sliderLoc='+sliderLoc+'; totalImgsWidth='+totalImgsWidth*/);
                   $(id_block).find('.lsc_slider-nav').show();
                   //disable next button if there are insuficient products
                  if (imgsLen>bigSlide) {
@@ -30,14 +34,14 @@
                      $(id_block).find('.lsc_previous_b').prop("disabled",true);
                  }
           },100);
-        });
+    //    });
     }
     function adjustCarousel(imgWidth,bigContainer,smallContainer) { //needs to be called when showing the div(in dropup carousel dev)             
               var bigcontainerWidth=bigContainer.filter(":visible").first().width(); //get the inner width without padding
               var containerWidth=smallContainer.filter(":visible").first().width();
               console.log('initial bigcontainerWidth='+bigcontainerWidth);
               console.log('initial containerWidth='+containerWidth);
-              var bigSlide2=parseInt(bigcontainerWidth/imgWidth); //add parseInt here
+              var bigSlide2=parseInt(bigcontainerWidth/imgWidth); 
               console.log('bigSlide2='+bigSlide2);
               var adjust=(bigSlide2*imgWidth)+15;
               console.log('adjust='+adjust);
@@ -50,11 +54,16 @@
                   return 1;
               }
           }
-    
-         carousel_footer(id_recently_click,id_recently_block,$('.ls_recent_carousel .lsc_wrap'),$('.ls_recent_carousel .lsc_slider')); //recently carousel
-         carousel_footer(id_favorite_click,id_favorite_block,$('.ls_preferate_carousel .lsc_wrap'),$('.ls_preferate_carousel .lsc_slider')); //favorites carousel
+    $(id_recently_click).on('click', function(){
+        carousel_footer(id_recently_click,id_recently_block,$('div.ls_recent_carousel').find('div.lsc_wrap').first(),$('div.ls_recent_carousel').find('div.lsc_slider').first()); //recently carousel
+    });
+     $(id_favorite_click).on('click', function(){
+        carousel_footer(id_favorite_click,id_favorite_block,$('div.ls_preferate_carousel').find('div.lsc_wrap').first(),$('div.ls_preferate_carousel').find('div.lsc_slider').first()); //favorites carousel
+    });
+         
+      
          //update navigation when deleting items
-        $('.lsc_next_b').on('click',function(){
+        $('body').on('click','.lsc_next_b',function(){
      //     console.log('forward, imgsLen='+imgsLen);
           sliderLoc=sliderLoc+bigSlide;
           leftMargin=parseInt(sliderUl.css('marginLeft'));
@@ -68,16 +77,19 @@
             console.log('sliderLoc='+sliderLoc);
             console.log('imgsLen-bigSlide='+(imgsLen-bigSlide));
     });
-     $('.lsc_previous_b').on('click',function(){
-    //      console.log('backward, imgsLen'+imgsLen);
+     $('body').on('click','.lsc_previous_b',function(){
           sliderLoc=sliderLoc-bigSlide;
+          console.log('backward, imgsLen: '+imgsLen+'; bigSlide:'+bigSlide+'; sliderLoc:'+sliderLoc);
           leftMargin=parseInt(sliderUl.css('marginLeft'));
           sliderUl.animate({
               'margin-left': leftMargin+(imgWidth*bigSlide)
             })
-           if (sliderLoc<(imgsLen-bigSlide)) {
+           if (sliderLoc<=(imgsLen-bigSlide)) {
                 $('.lsc_next_b').prop("disabled",false);
-            } 
+             
+            } else {
+                console.log('next still disabled');
+            }
             if (sliderLoc==1) {
                 $('.lsc_previous_b').prop("disabled",true);
             }
@@ -85,16 +97,18 @@
      //       console.log('leftMargin='+parseInt(sliderUl.css('marginLeft')));
     });
     
-     function update_nav_carousel(id_block){
+     function update_nav_carousel(id_block){  
          imgs=$(id_block).find('.lsc_li_container').filter(":visible");
-         imgsLen=imgs.length-1;
+         imgsLen=imgs.length;
          if (sliderLoc>(imgsLen-bigSlide)) {
                 $('.lsc_next_b').prop("disabled",true);
             }
-        console.log('update_nav_carousel: sliderLoc='+sliderLoc+';imgsLen='+imgsLen+';bigSlide='+bigSlide);
+        console.log('update_nav_carousel: sliderLoc='+sliderLoc+';imgsLen='+imgsLen+';bigSlide='+bigSlide+';id_block: '+id_block);
      }
  $('body').on('click',' div.ls_mid_myaccount a.ty-twishlist-item__remove.ty-remove',function() { 
-           check_block();
+      setTimeout(function () {
+            check_block();
+        }, 1500); //wait for ajax call and the subsquent removed item from frontend (replace with callback function)
  });
      function check_block() {
          if ($(id_favorite_block).is(':visible')){
